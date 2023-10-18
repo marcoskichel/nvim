903d4042932f10a40936431a752cbb84c7afccea
Marcos Kichel <marcos.kichel@gmail.com>
2023-10-18 03:34:57 +0000

    Add description to custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Recent files
+vim.keymap.set("n", "<C-r>", "<leader>fb", { remap = true, desc = "Open recent buffers dialog" })
+
+-- Search for first occurrence of word under cursor
+vim.keymap.set("n", "<C-o>", "s", { remap = true, desc = "Search for first occurrence of word under cursor" })
+
+-- Window navigation
+vim.keymap.set("n", "<leader>wl", "<leader>w|", { remap = true, desc = "Split window right" })
+vim.keymap.set("n", "<leader>wj", "<leader>w-", { remap = true, desc = "Split window below" })

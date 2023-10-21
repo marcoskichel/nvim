474a17315c8f40f1c5255c1222da89528f63b419
Marcos Kichel <marcos.kichel@gmail.com>
2023-10-21 05:15:37 +0000

    Add sqlfmt when saving SQL files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local autocmd_group = vim.api.nvim_create_augroup("custom", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.sql" },
+  desc = "Auto-format SQL files after saving",
+  callback = function()
+    local fileName = vim.api.nvim_buf_get_name(0)
+    vim.cmd(":!sqlfmt " .. fileName)
+  end,
+  group = autocmd_group,
+})

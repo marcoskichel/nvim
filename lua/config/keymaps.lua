93a73088a29b85ab882faf2a8c5f8e7aa625ec11
Marcos Kichel <marcos.kichel@gmail.com>
2023-10-21 05:15:47 +0000

    Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,12 +3,12 @@
 -- Add any additional keymaps here
 
 -- Recent files
-vim.keymap.set("n", "<C-r>", "<leader>fb", { remap = true, desc = "Open recent buffers dialog" })
+-- vim.keymap.set("n", "<C-r>", "<leader>fb", { remap = true, desc = "Open recent buffers dialog" })
 
 -- Window navigation
 vim.keymap.set("n", "<leader>wl", "<leader>w|", { remap = true, desc = "Split window right" })
 vim.keymap.set("n", "<leader>wj", "<leader>w-", { remap = true, desc = "Split window below" })
-vim.keymap.set({ "n", "i", "v" }, "<C-k>", "<leader>ff", { remap = true, desc = "Find files (current directory)" })
+-- vim.keymap.set({ "n", "i", "v" }, "<C-k>", "<leader>fF", { remap = true, desc = "Find files (current directory)" })
 
 -- Redo
 vim.keymap.set({ "n" }, "<C-y>", ":redo<cr>", { noremap = true, silent = true, desc = "Redo" })
@@ -20,16 +20,13 @@ vim.keymap.set({ "n", "i" }, "<C-a>", "<esc>ggVG<cr>", { noremap = true, silent 
 vim.keymap.set({ "n" }, "<C-A-Down>", "Yp", { noremap = true, silent = true, desc = "Duplicate line" })
 vim.keymap.set({ "v" }, "<C-A-Down>", "Y`>p", { noremap = true, silent = true, desc = "Duplicate line" })
 
---Delete content
-vim.keymap.set({ "n" }, "<C-d>", "dd", { remap = true, silent = true, desc = "Delete current line" })
-
 -- Spectre search and replace
 vim.keymap.set(
   "n",
-  "<C-f>",
+  "<C-S-f>",
   "<cmd>lua require('spectre').open_file_search({})<CR>i",
   { remap = true, silent = true, desc = "Search in current buffer" }
 )
 
--- Replace find with leap
-vim.keymap.set("n", "f", "s", { remap = true, desc = "Leap" })
+-- Aerial
+vim.keymap.set("n", "<C-o>", "<cmd>AerialToggle!<CR>")

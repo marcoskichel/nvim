6b90ca97875f2511919dfffbae79c4ccc2139e38
Marcos Kichel <marcos.kichel@gmail.com>
2023-10-23 20:54:14 +0000

    Fix copilot issues

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,7 @@ return {
   cmd = "Copilot",
   build = ":Copilot auth",
   opts = {
+    suggestion = { enabled = true },
     filetypes = {
       markdown = true,
       yaml = true,
@@ -37,6 +38,23 @@ return {
         group_index = 1,
         priority = 100,
       })
+    end,
+  },
+
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = "copilot.lua",
+    opts = {},
+    config = function(_, opts)
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup(opts)
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      require("lazyvim.util").lsp.on_attach(function(client)
+        if client.name == "copilot" then
+          copilot_cmp._on_insert_enter({})
+        end
+      end)
     end,
   },
 }
